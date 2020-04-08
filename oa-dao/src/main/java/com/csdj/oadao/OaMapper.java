11b81acaa45250dbe887a5269b6f2b886856821b
pxy <3203604400@qq.com>
2020-04-08 16:24:40 +0800

    第一次

---
+++ oa-dao/src/main/java/com/csdj/oadao/OaMapper.java
@@ -0,0 +1,8 @@
+package com.csdj.oadao;
+
+import com.csdj.pojo.Faqs;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OaMapper extends tk.mybatis.mapper.common.Mapper<Faqs> {
+}
---
+++ oa-dao/src/test/java/com/csdj/oadao/OaDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csdj.oadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
